7498ea434fbc485eec883447cdc57fb0e29baa55
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-12 16:04:42 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "resourcename" {
+  default = "k8s-resources"
+}
+variable "clustername" {
+  default = "kubernetes-aks1"
+}
+variable "location" {
+  default = "East US"
+}
+variable "dnspreffix" {
+  default = "kubecluster"
+}
+variable "size" {
+  default = "Standard_D2_v2"
+}
+variable "agentnode" {
+  default = "1"
+}
+variable "subscription_id" {
+  default = "06a981a6-ce1c-421e-a6e8-431e5dcd12af"
+}
+variable "client_id" {
+  default = "1f13ac3a-048c-40ae-a965-148669cdfef8"
+}
+variable "client_secret" {
+  default = "xU-8Q~V8QEt5mBXAkSMpCDcbtArymTYCAHXeca03"
+}
+variable "tenant_id" {
+  default = "a6eec4e9-2344-4673-8365-0641c7db9a48"
+}
