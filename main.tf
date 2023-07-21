50837f198b51047e266145c3bed9385eec2764dc
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-21 10:27:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 provider "azurerm" {
   #subscription_id = var.subscription_id
   #client_id       = var.client_id
