a113a79af2efd4cb3b1ee2a0b53f99352c1028ac
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-21 10:58:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,17 +16,21 @@ variable "size" {
 variable "agentnode" {
   default = "1"
 }
-#variable "subscription_id" {
-#  default = "06a981a6-ce1c-421e-a6e8-431e5dcd12af"
-#}
-#variable "client_id" {
-#  default =  "1f13ac3a-048c-40ae-a965-148669cdfef8"    #$(client_id)     #
-#  type        = string
-#}
-#variable "client_secret" {
-#  default = "xU-8Q~V8QEt5mBXAkSMpCDcbtArymTYCAHXeca03"    #$(client_secret)         #
-#  type        = string
-#}
-#variable "tenant_id" {
+variable "subscription_id" {
+  #default = "06a981a6-ce1c-421e-a6e8-431e5dcd12af"
+  description = "Azure Tenant ID"
+}
+variable "client_id" {
+  #default =  "1f13ac3a-048c-40ae-a965-148669cdfef8"    #$(client_id)     #
+  #type        = string
+  description = "Azure Tenant ID"
+}
+variable "client_secret" {
+ # default = "xU-8Q~V8QEt5mBXAkSMpCDcbtArymTYCAHXeca03"    #$(client_secret)         #
+  #type        = string
+  description = "Azure Tenant ID"
+}
+variable "tenant_id" {
 #  default =  "a6eec4e9-2344-4673-8365-0641c7db9a48"    #$(tenant_id)   #
-#}
+  description = "Azure Tenant ID"
+}
