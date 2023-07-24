915ca3ae952e385f93e71ccbc06c2f528a50fa96
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-24 13:33:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 provider "azurerm" {
   subscription_id = "$(subscription_id)"
-  client_id       = ""$(client_id)""
+  client_id       = "$(client_id)"
   client_secret   = "$(client_secret)"
   tenant_id       = var.tenant_id
   features {}
