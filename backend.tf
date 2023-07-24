a62a1deca2b82dc9b820b57718feef7398f65659
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2023-07-24 12:17:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,9 +4,9 @@ terraform {
     storage_account_name = "tstateterraform01"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    subscription_id      =   var.subscription_id
-    client_id            =   var.client_id
-    client_secret        =   var.client_secret
-    tenant_id            =   var.tenant_id
+    subscription_id      =   "$(subscription_id)"
+    client_id            =   "$(client_id)"
+    client_secret        =   "$(client_secret)"
+    tenant_id            =   "$(tenant_id)"
   }
 }
